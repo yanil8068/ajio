4a6e0ce956b030d1482a7b87eafa53e80e8ea0ae
Ajay Yadav <118597153+Ajaybipul@users.noreply.github.com>
2024-09-07 11:56:59 +0530

    Update Store.js
    update store file

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import allProductReducer  from "../Redux/Electronics/allProductSlice";
 import usersReducer from "../Redux/Authentication/usersSlice";
+import mensReducer from '../Redux/Mens/Mens';
+import jewelleryReducer from '../Redux/Jewellery/Jewellery';
+import cartReducer from '../Redux/Cart/Cart'
+import womensReducer from '../Redux/Womens/Womens';
+import WishlistReducer from '../Redux/Wishlist/Wishlist';
 
 export default configureStore({
   reducer: {
     users: usersReducer,
     allElectronicsProduct: allProductReducer,
+    mens: mensReducer,
+    jewellery: jewelleryReducer,
+    cart: cartReducer,
+    womens:womensReducer,
+    wishlist:WishlistReducer
   },
 });
