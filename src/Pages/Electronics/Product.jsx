e6816c80729e2f74e197ffdc9e0c31a3b42396d5
Yogesh-Ginti <yogeshginti@gmail.com>
2024-09-08 01:29:10 +0530

    addToCart and addToWishlist done for electronics products

--- src/Pages/Electronics/Product.jsx
+++ src/Pages/Electronics/Product.jsx
@@ -1,5 +1,8 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { auth } from "../../firebase/config";
+import { useSelector, useDispatch} from "react-redux";
+import { addToCart } from "../../Redux/Cart/Cart";
+import { addToWishlist } from "../../Redux/Wishlist/Wishlist";
 import { useState, useEffect } from "react";
 import ProductImage from "../../Components/Electronics/ProductImage";
 import ProductDetails from "../../Components/Electronics/ProductDetails";
@@ -8,12 +11,14 @@ import SimilarProducts from "../../Components/Electronics/SimilarProducts";
 
 function Product() {
   const { id } = useParams();
+  const dispatch = useDispatch()
   const navigate = useNavigate();
   const { allElectronicsProduct } = useSelector((state) => state.allElectronicsProduct);
   const [similarProducts, setSimilarProducts] = useState([]);
   const [selectedSize, setSelectedSize] = useState(null);
-
   const product = allElectronicsProduct.find((item) => item.id === parseInt(id));
+   // Optional: To check if item is already in wishlist
+   const wishlistItems = useSelector((state) => state.wishlist.items);
 
   useEffect(() => {
     const fetchSimilarProducts = () => {
@@ -30,8 +35,52 @@ function Product() {
     navigate(`/electronics/products/${similarProductId}`);
   };
 
-  const handleSizeClick = (size) => {
-    setSelectedSize(size);
+
+  const handleAddToCart = () => {
+    if (!auth.currentUser) {
+      alert("Please login your account first!");
+      return;
+    }
+
+    const cartItem = {
+      id: product.id,
+      title: product.title,
+      image: product.image,
+      price: product.price,
+      quantity: 1,
+      size: selectedSize,
+    };
+
+    dispatch(addToCart(cartItem));
+    alert(`${product.title} has been added to your cart!`);
+  };
+
+  const handleAddToWishlist = () => {
+    if (!auth.currentUser) {
+      alert("Please login your account first!");
+      return;
+    }
+
+    const wishlistItem = {
+      id: product.id,
+      title: product.title,
+      image: product.image,
+      price: product.price,
+      size: selectedSize,
+    };
+
+    // Check if item is already in wishlist
+    const isItemInWishlist = wishlistItems.some(
+      (item) => item.id === product.id && item.size === selectedSize
+    );
+
+    if (isItemInWishlist) {
+      alert("This item is already in your wishlist.");
+      return;
+    }
+
+    dispatch(addToWishlist(wishlistItem));
+    alert(`${product.title} has been added to your wishlist!`);
   };
 
   if (!product) {
@@ -45,9 +94,10 @@ function Product() {
       <div className="flex flex-col lg:flex-row gap-6 mb-8">
         <ProductImage image={product.image} title={product.title} />
         <ProductDetails
+          handleAddToCart={handleAddToCart}
+          handleAddToWishlist={handleAddToWishlist}
           product={product}
           selectedSize={selectedSize}
-          handleSizeClick={handleSizeClick}
         />
       </div>
       <RatingGraph rating={product.rating} />
