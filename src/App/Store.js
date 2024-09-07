bc81d019229531a52ba35805f474e12427a400e2
Yogesh ginti <163818028+Yogesh-Ginti@users.noreply.github.com>
2024-09-07 11:52:13 +0530

    Update Store.js

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import allProductReducer  from "../Redux/Electronics/allProductSlice";
 import usersReducer from "../Redux/Authentication/usersSlice";
 
 export default configureStore({
   reducer: {
     users: usersReducer,
+    allElectronicsProduct: allProductReducer,
   },
 });
